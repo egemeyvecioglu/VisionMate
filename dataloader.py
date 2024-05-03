163d72310ddb5c91928ae03425d1c7c847c2dcb3
egemeyvecioglu <ege.meyvecioglu@metu.edu.tr>
2024-05-03 17:20:20 +0300

    predicted and grout truth gaze direction visualization added

--- dataloader.py
+++ dataloader.py
@@ -5,7 +5,9 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 import torch
 from PIL import Image
-from torchvision import transforms
+from torchvision import transforms, models
+import utils
+from model import FastViTMLP, FasterViT, FastViTL2CS, ResNET50L2CS, FastViTL2CS
 
 class BaseDataset(Dataset):
     def __init__(self, data_dir, transform=None):
@@ -127,24 +129,76 @@ class Gaze360Dataset(BaseDataset):
 if __name__ == "__main__":
 
     # Example usage
-    data_dir = "./data/MPIIFaceGaze"
-    dataset = MPIIFaceGazeDataset(data_dir)
+    data_dir = "/home/kovan-beta/Desktop/visionmate/datasets/Gaze360/"
+    
+    val_transform = transforms.Compose([
+        transforms.Resize((448, 448)),
+        transforms.ToTensor(),
+        # transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
 
-    # Split dataset into train and test sets
-    # train_dataset, test_dataset = train_test_split(dataset, test_size=0.2, random_state=42)
+    dataset = Gaze360Dataset(data_dir, "train.txt", transform=val_transform)
+    test_dataset = Gaze360Dataset(data_dir, "test.txt", transform=val_transform)
 
-    # print(len(train_dataset))  # Output: 1200
-    # print(len(test_dataset))  # Output: 300
+    # dataset = MPIIFaceGazeDataset("/home/kovan-beta/Desktop/visionmate/datasets/MPIIFaceGaze", "p00", train=True, transform=None)
+    # test_dataset = MPIIFaceGazeDataset("/home/kovan-beta/Desktop/visionmate/datasets/MPIIFaceGaze", "p00", train=False, transform=val_transform)    
 
-    # Create dataloaders for train and test sets
-    train_dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
-    # test_dataloader = DataLoader(test_dataset, batch_size=32, shuffle=False)
+    # dataset = MPIIFaceGazeProcessedDataset("/home/kovan-beta/Desktop/visionmate/datasets/MPIIFaceGazeProcessed", "p00", train=True, transform=val_transform)
+    # test_dataset = MPIIFaceGazeProcessedDataset("/home/kovan-beta/Desktop/visionmate/datasets/MPIIFaceGazeProcessed", "p00", train=False, transform=val_transform)
 
-    for images, gaze_directions in train_dataloader:
-        print("Train:", images.shape, gaze_directions.shape)
-        break
+    train_dataloader = DataLoader(dataset, batch_size=1, shuffle=True)
+    test_dataloader = DataLoader(test_dataset, batch_size=1, shuffle=False)
 
-    # for images, gaze_directions in test_dataloader:
-    #     print("Test:", images.shape, gaze_directions.shape)
-    #     break
+    model1 = FasterViT("cuda")
+    state = torch.load("/home/kovan-beta/Desktop/visionmate/VisionMate/models/fastervit_gaze360-2.pth")
+    model1.load_state_dict(state["state_dict"])
+    model1.eval()
 
+    model2 = ResNET50L2CS("cuda", 90, models.resnet.Bottleneck, [3, 4, 6, 3])
+    model2.load_state_dict(torch.load("/home/kovan-beta/L2CS-Net/models/L2CSNet_gaze360.pkl"))
+    model2.eval()
+
+    for images, gaze_directions in test_dataloader:
+
+        images_cuda, gaze_directions_cuda = images.to("cuda"), gaze_directions.to("cuda")
+
+        if isinstance(model1, ResNET50L2CS):
+            #resize the image to 448
+            model1_pred = utils.L2CS_final_output(model1, images_cuda)
+            model1_pred  = model1_pred[0][0], model1_pred[1][0]
+        else:
+            images_cuda = torch.nn.functional.interpolate(images_cuda, size=(224,224), mode="bilinear")
+            outputs = model1(images_cuda)
+            model1_pred = outputs[:, 0].item(), outputs[:, 1].item()
+
+        
+        if isinstance(model2, ResNET50L2CS):
+            #resize the image to 448
+            # images_cuda = torch.nn.functional.interpolate(images_cuda, size=(448,448), mode="bilinear")
+            model2_pred = utils.L2CS_final_output(model2, images_cuda)
+            model2_pred  = model2_pred[0][0], model2_pred[1][0]
+            print(model2_pred)
+        else:
+            outputs = model2(images_cuda)
+            model2_pred = outputs[:, 0].item(), outputs[:, 1].item()
+
+        #display the image in opencv and draw the gaze direction
+        image = images[0].permute(1, 2, 0).numpy()
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        gaze = gaze_directions[0].numpy()
+
+        #resize the image to 448
+        image = cv2.resize(image, (448, 448))
+
+        x1, y1, x2, y2 = 0, 0, image.shape[1], image.shape[0]
+
+        pitch = gaze[0]
+        yaw = gaze[1]
+        utils.draw_gaze(x1, y1, x2, y2, image, model1_pred, color=(0, 0, 255)) 
+        utils.draw_gaze(x1, y1, x2, y2, image, model2_pred, color=(255, 0, 0))
+        utils.draw_gaze(x1, y1, x2, y2, image, gaze, color=(0, 255, 0))
+
+
+        cv2.imshow("image", image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
