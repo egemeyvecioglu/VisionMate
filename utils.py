0d56f5d05a5f19b2db30e82d424fa40e82c39a04
egemeyvecioglu <ege.meyvecioglu@metu.edu.tr>
2024-05-03 17:19:47 +0300

    new utility functions

--- utils.py
+++ utils.py
@@ -1,4 +1,23 @@
 import torch
+import numpy as np
+import cv2
+
+def L2CS_final_output(model, image):
+
+    gaze_pitch, gaze_yaw = model(image)
+
+    pitch_predicted = model.softmax(gaze_pitch)
+    yaw_predicted = model.softmax(gaze_yaw)
+    
+    # Get continuous predictions in degrees.
+    pitch_predicted = torch.sum(pitch_predicted.data * model.idx_tensor, dim=1) * 4 - 180
+    yaw_predicted = torch.sum(yaw_predicted.data * model.idx_tensor, dim=1) * 4 - 180
+    
+    pitch_predicted= pitch_predicted.cpu().detach().numpy()* np.pi/180.0
+    yaw_predicted= yaw_predicted.cpu().detach().numpy()* np.pi/180.0
+
+    return pitch_predicted, yaw_predicted
+
 
 def spherical2cartesial(x):
 
@@ -25,6 +44,20 @@ def compute_angular_error(input, target):
 
     return mean_angle
 
+def draw_gaze(a,b,c,d,image_in, pitchyaw, thickness=2, color=(255, 255, 0),sclae=2.0):
+    """Draw gaze angle on given image with a given eye positions."""
+    image_out = image_in
+    (h, w) = image_in.shape[:2]
+    length = c
+    pos = (int(a+c / 2.0), int(b+d / 2.0))
+    if len(image_out.shape) == 2 or image_out.shape[2] == 1:
+        image_out = cv2.cvtColor(image_out, cv2.COLOR_GRAY2BGR)
+    dx = -length * np.sin(pitchyaw[0]) * np.cos(pitchyaw[1])
+    dy = -length * np.sin(pitchyaw[1])
+    cv2.arrowedLine(image_out, tuple(np.round(pos).astype(np.int32)),
+                   tuple(np.round([pos[0] + dx, pos[1] + dy]).astype(int)), color,
+                   thickness, cv2.LINE_AA, tipLength=0.18)
+    return image_out
 
 class AverageMeter(object):
     """Computes and stores the average and current value"""
